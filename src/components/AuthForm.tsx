55397253b8e5bac08d37e24ca6689888ff11aebe
maitri-dalvi <maitridalvi2004@gmail.com>
2025-04-26 23:56:53 +0530

    changed folder struct

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -12,6 +12,8 @@ import {Form} from "@/components/ui/form"
 import { toast } from "sonner"
 import FormField from "./FormField"
 import { useRouter } from "next/navigation"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase/client"
 
 
 const authFormSchema = (type: FormType) => {
@@ -41,9 +43,27 @@ const AuthForm = ({ type }: { type:  FormType}) => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if(type === 'sign-up') {
+
+        const { name, email, password } = values;
+
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+        const result = await signUp(
+          {
+            uid: userCredentials.user.uid,
+            name: name!, 
+            email,
+            password,
+          }
+        )
+        if(!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+
         toast.success('Account created successfully. Please sign in.');
         router.push('/sign-in')
       }
