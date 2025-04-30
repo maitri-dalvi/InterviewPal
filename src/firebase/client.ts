d8ffc78ec96fde51a81a7bf38a82ff6488b453fd
maitri-dalvi <maitridalvi2004@gmail.com>
2025-04-30 21:40:00 +0530

    firebase err

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcSPKGXqk54SdtzqDKodG1gHoWTKLcdkQ",
-  authDomain: "interviewpal-7964c.firebaseapp.com",
-  projectId: "interviewpal-7964c",
-  storageBucket: "interviewpal-7964c.firebasestorage.app",
-  messagingSenderId: "1025823635124",
-  appId: "1:1025823635124:web:ccd3a254f196d6c34d49e1",
-  measurementId: "G-7HS6WN9ZLQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
