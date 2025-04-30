0d59ca1ebb0b6d2087cf2d434ba16abfde1a25b3
maitri-dalvi <maitridalvi2004@gmail.com>
2025-04-30 13:27:25 +0530

    implemented interview session

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { vapi } from "@/lib/vapi.sdk";
+import {interviewer} from "@/constants"
 
 
 enum CallStatus {
@@ -19,7 +20,7 @@ interface SavedMessage {
   content: string;
 }
 
-const Agent = ({userName, userId, type}: AgentProps) => {
+const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => {
     const router = useRouter();
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
     const [messages, setMessages] = useState<SavedMessage[]>([]);
@@ -73,24 +74,63 @@ const Agent = ({userName, userId, type}: AgentProps) => {
         vapi.off("error", onError);
       };
     }, []);
+
+    const handleGenerateFeedback = async (messgaes: SavedMessage[]) => {
+      console.log('Generate feedback here.');
+      
+      //TODO -> Create a server action that generates feedback
+      const {success, id} = {
+        success: true,
+        id: 'feedback-id'
+      }
+
+      if(success && id) {
+        router.push(`/interview/${interviewId}/feedback`);
+      } else {
+        console.log('Error saving feedback');
+        router.push('/');
+        
+      }
+      
+    }
     
     useEffect(() => {
-      if (callStatus === CallStatus.FINISHED) {
-          router.push("/");
+      if(callStatus === CallStatus.FINISHED) {
+        if(type === 'generate') {
+          router.push('/')
+        } else {
+          handleGenerateFeedback(messages);
+        }
       }
+      
     }, [messages, callStatus, type, userId]);
   
   
     const handleCall = async () => {
       setCallStatus(CallStatus.CONNECTING);
-  
+      
+      if (type === 'generate') {
+      
         await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
           variableValues: {
             username: userName,
             userid: userId,
           },
         });
+      } else {
+        let formattedQuestions = '';
+
+        if(questions) {
+          formattedQuestions = questions.map((question) => `- ${question}`)
+          .join('\n');
+        }
+        await vapi.start('interviewer', {
+          variableValues: {
+            questions: formattedQuestions
+          }
+        })
       }
+    }
   
     const handleDisconnect = () => {
       setCallStatus(CallStatus.FINISHED);
--- src/components/InterviewCard.tsx
+++ src/components/InterviewCard.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { Button } from './ui/button';
 import Link from 'next/link';
 
-const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
 
@@ -45,8 +45,8 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
             <Link
               href={
                 feedback
-                  ? `/interview/${interviewId}/feedback`
-                  : `/interview/${interviewId}`
+                  ? `/interview/${id}/feedback`
+                  : `/interview/${id}`
               }
             >
               {feedback ? "Check Feedback" : "View Interview"}
